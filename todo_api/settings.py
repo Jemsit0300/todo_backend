40b7bb435194d6911866fd57ebb395da6a3f37da
Bagtiyar Rejepov <149511416+noxur05@users.noreply.github.com>
2025-10-22 09:11:52 +0500

    Configure SIMPLE_JWT token lifetimes
    Added SIMPLE_JWT settings for token lifetimes.

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,6 +157,14 @@ REST_FRAMEWORK = {
        'django_filters.rest_framework.DjangoFilterBackend'
    ],
 }
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+
+}
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
